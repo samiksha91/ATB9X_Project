7ee8845c99de7e96e38ab78804c3996728e45823
Samiksha Naidu <sami@sozodesign.co.uk>
2024-10-16 20:52:21 +0530

    commit and push

---
+++ src/Task_09102024/test.java
@@ -0,0 +1,11 @@
+package Task_09102024;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("Number =" +a);
+         int a1 = -a;
+        System.out.println("Number =" +a1);
+
+    }
+}
